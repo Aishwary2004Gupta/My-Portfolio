becdfe65d2a9d347c69774f08b4c0cdc78478ae5
Aishwary Gupta <aish042004@gmail.com>
2024-12-15 09:48:32 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message');
 
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    //service id, template id, #form publickey
+    emailjs.sendForm('', '', '')
+}
+contactForm.addEventListener('submit', sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
