670b2bd4ab8562c0fb4b2ba4cc11cad75c2e87b1
Aishwary Gupta <aish042004@gmail.com>
2024-12-15 10:46:10 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,31 @@
 /*=============== EMAIL JS ===============*/
+// Get the form and message elements
 const contactForm = document.getElementById('contact-form'),
     contactMessage = document.getElementById('contact-message');
 
+// Function to handle email sending
 const sendEmail = (e) => {
     e.preventDefault();
 
-    //service id, template id, #form, publickey
+    // Send form data via EmailJS
     emailjs.sendForm('service_a363o6c', 'template_n0nc739', '#contact-form', 'Ms7jlE298nKUS26Pm')
-
     .then(() => {
+        // Success message
         contactMessage.textContent = 'Message sent successfully! 🫡';
 
+        // Clear message after 5 seconds
         setTimeout(() => {
-            contactMessage.textContent = ''
-        }, 5000);
+            contactMessage.textContent = '';
+        }, 5000)
 
+        // Reset the form
         contactForm.reset();
     })
-}
+};
+
+// Add event listener for form submission
 contactForm.addEventListener('submit', sendEmail);
+
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
