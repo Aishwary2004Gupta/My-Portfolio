15338a817b47df2c1190cc43b388837599be2d45
Aishwary Gupta <aish042004@gmail.com>
2024-12-15 10:21:17 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,18 @@ const contactForm = document.getElementById('contact-form'),
 const sendEmail = (e) => {
     e.preventDefault();
 
-    //service id, template id, #form publickey
-    emailjs.sendForm('', '', '')
+    //service id, template id, #form, publickey
+    emailjs.sendForm('service_a363o6c', 'template_n0nc739', '#contact-form', 'Ms7jlE298nKUS26Pm')
+
+    .then(() => {
+        contactMessage.textContent = 'Message sent successfully! 🫡';
+
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000);
+
+        contactForm.reset();
+    })
 }
 contactForm.addEventListener('submit', sendEmail);
 
