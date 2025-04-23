7471c1a50a8e2c2fa3d0c7fdd8ff87fe5108369b
Aishwary Gupta <aish042004@gmail.com>
2025-04-24 00:00:58 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,53 +9,53 @@ const sendEmail = (e) => {
 
     // Send form data via EmailJS
     emailjs.sendForm('service_a363o6c', 'template_vk5lomp', '#contact-form', 'Ms7jlE298nKUS26Pm')
-    .then(() => {
-        // Success message
-        contactMessage.textContent = 'Message sent successfully! ✅';
+        .then(() => {
+            // Success message
+            contactMessage.textContent = 'Message sent successfully! ✅';
 
-        // Clear message after 5 seconds
-        setTimeout(() => {
-            contactMessage.textContent = '';
-        }, 5000)
+            // Clear message after 5 seconds
+            setTimeout(() => {
+                contactMessage.textContent = '';
+            }, 5000)
 
-        // Reset the form
-        contactForm.reset();
-    }, () =>{
-        contactMessage.textContent = 'Message not sent (service error) ❌';
-    })
+            // Reset the form
+            contactForm.reset();
+        }, () => {
+            contactMessage.textContent = 'Message not sent (service error) ❌';
+        })
 };
 
 // Add event listener for form submission
 contactForm.addEventListener('submit', sendEmail);
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
-const scrollUp = () =>{
-	const scrollUp = document.getElementById('scroll-up')
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						: scrollUp.classList.remove('show-scroll')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
-    
-const scrollActive = () =>{
-  	const scrollDown = window.scrollY
 
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__list a[href*=' + sectionId + ']')
+const scrollActive = () => {
+    const scrollDown = window.scrollY
 
-		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__list a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
 }
 window.addEventListener('scroll', scrollActive)
 
@@ -70,7 +70,7 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.perfil, .contact__form`)
-sr.reveal(`.info`, {origin: 'left', delay: 800})
-sr.reveal(`.skills`, {origin: 'left', delay: 1000})
-sr.reveal(`.about`, {origin: 'right', delay: 1200})
-sr.reveal(`.projects__card, .coding__card, .experience__card`, {interval: 100})
+sr.reveal(`.info`, { origin: 'left', delay: 800 })
+sr.reveal(`.skills`, { origin: 'left', delay: 1000 })
+sr.reveal(`.about`, { origin: 'right', delay: 1200 })
+sr.reveal(`.projects__card, .coding__card, .experience__card`, { interval: 100 })
